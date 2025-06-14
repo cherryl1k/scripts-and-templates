0d5545ee0da1332fcc80ee8cc272b69c46887192
chrymusic <102343489+chrymusic@users.noreply.github.com>
2025-06-14 12:34:15 -0500

    minor fixes

--- templates/build_scripts/renoise_tool_build.bat
+++ templates/build_scripts/renoise_tool_build.bat
@@ -1,12 +1,18 @@
 @echo off
 set directory=src
-set output=tool
+set output=com.timesigchange
 
-:: Compress src into tool.zip
-tar -a -c -f %output%.zip %directory%
+:: Remove previous files if they exist
+if exist %output%.xrnx del %output%.xrnx
+if exist %output%.zip del %output%.zip
 
-:: Rename tool.zip to tool.xrnx
+:: Compress src's content directly into a .zip
+cd %directory%
+tar -a --options compression-level=0 -c -f ../%output%.zip *
+cd ..
+
+:: Rename .zip to .xrnx
 rename %output%.zip %output%.xrnx
 
-echo Done! %output%.xrnx has been created.
+echo Done! %output%.xrnx has been created, replacing any previous files.
 pause
